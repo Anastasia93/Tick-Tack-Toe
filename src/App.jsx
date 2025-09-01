06e3da1e02dd9dd2799c252452fa7e5faa5fa1e4
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-09-01 15:16:48 +0200

    Adds Player name input and basic interaction.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import Player from "./components/Player.jsx"
 
 function App() {
+  
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="X"/>
-          <Player name="Player 2" symbol="O"/>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
       </div>
     </main>
