143bffce8b2b6a5d3cf0dbb4811d019088741947
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-09-04 14:43:02 +0200

    Adds logging logic.

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
-export default function Log() {
+export default function Log({ turns }) {
     return <ol id='log'>
+        {turns.map(turn => {
+            const row = turn.square.row;
+            const column = turn.square.column;
 
+            return <li key={`${row}${column}`}>{turn.player} selected {row},{column}</li>
+        })}
     </ol>
 }
