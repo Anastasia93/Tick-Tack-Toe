f7e0148204bae9480ae9e7cf02b446cd00574600
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-09-04 15:04:56 +0200

    Updates the game turns flow.

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ import GameBoard from "./components/GameBoard.jsx"
 import Player from "./components/Player.jsx"
 import Log from "./components/Log.jsx"
 
-function App() {
-  const [gameTurns, saveGameTurns] = useState([]);
-  
+function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
 
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
     currentPlayer = 'O';
   }
 
+  return currentPlayer;
+}
+
+function App() {
+  const [gameTurns, saveGameTurns] = useState([]);
+
+  const activePlayer = deriveActivePlayer(gameTurns)
+
   function handleSelectSquare(rowIndex, columnIndex) {
     saveGameTurns(previousTurns => {
+      const currentPlayer = deriveActivePlayer(previousTurns);
+
       const updatedTurns = [
         { square: { row: rowIndex, column: columnIndex }, player: currentPlayer },
         ...previousTurns
@@ -28,12 +36,12 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={currentPlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={currentPlayer === 'O'} />
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
   )
 }
