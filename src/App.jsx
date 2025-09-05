8c041d8776dc4bb1234b62ca6f61369e9e6accd1
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-09-05 14:47:38 +0200

    Adds winner player name feature.

--- src/App.jsx
+++ src/App.jsx
@@ -25,62 +25,72 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2',
+  });
   const [gameTurns, saveGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = [...initialGameBoard.map((array) => [...array] )];
-let winner;
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+  let winner;
 
-for (const turn of gameTurns) {
-  const { square, player } = turn;
-  const { row, column } = square;
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, column } = square;
 
-  gameBoard[row][column] = player;
-}
-
-for (const combination of WINNING_COMBINATIONS) {
-  const firstWinningSquare = gameBoard[combination[0].row][combination[0].column];
-  const secondWinningSquare = gameBoard[combination[1].row][combination[1].column];
-  const thirdWinningSquare = gameBoard[combination[2].row][combination[2].column];
-
-  if (firstWinningSquare && firstWinningSquare === secondWinningSquare && secondWinningSquare === thirdWinningSquare) {
-    winner = firstWinningSquare;
+    gameBoard[row][column] = player;
   }
-}
 
-const hasDraw = gameTurns.length === 9 && !winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstWinningSquare = gameBoard[combination[0].row][combination[0].column];
+    const secondWinningSquare = gameBoard[combination[1].row][combination[1].column];
+    const thirdWinningSquare = gameBoard[combination[2].row][combination[2].column];
 
-function handleSelectSquare(rowIndex, columnIndex) {
-  saveGameTurns(previousTurns => {
-    const currentPlayer = deriveActivePlayer(previousTurns);
+    if (firstWinningSquare && firstWinningSquare === secondWinningSquare && secondWinningSquare === thirdWinningSquare) {
+      winner = players[firstWinningSquare];
+    }
+  }
 
-    const updatedTurns = [
-      { square: { row: rowIndex, column: columnIndex }, player: currentPlayer },
-      ...previousTurns
-    ];
+  const hasDraw = gameTurns.length === 9 && !winner;
 
-    return updatedTurns;
-  });
-}
+  function handleSelectSquare(rowIndex, columnIndex) {
+    saveGameTurns(previousTurns => {
+      const currentPlayer = deriveActivePlayer(previousTurns);
 
-function handleRematch() {
-  saveGameTurns([]);
-}
+      const updatedTurns = [
+        { square: { row: rowIndex, column: columnIndex }, player: currentPlayer },
+        ...previousTurns
+      ];
 
-return (
-  <main>
-    <div id="game-container">
-      <ol id="players" className="highlight-player">
-        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
-      </ol>
-      {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch} />}
-      <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
-    </div>
-    <Log turns={gameTurns} />
-  </main>
-)
+      return updatedTurns;
+    });
+  }
+
+  function handleRematch() {
+    saveGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(previousPlayers => {
+      return { ...previousPlayers, [symbol]: newName }
+    })
+  }
+
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player onChangeName={handlePlayerNameChange} initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player onChangeName={handlePlayerNameChange} initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+        </ol>
+        {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch} />}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
+  )
 }
 
 export default App
