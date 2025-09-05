b767cff903ad49c50eda5606db4453bd76aba795
Anastasiia Yahniuk <anastasiia.yahniuk@gmail.com>
2025-09-05 13:38:52 +0200

    Adds Rematch logic.

--- src/App.jsx
+++ src/App.jsx
@@ -29,54 +29,58 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
-  let winner;
+  let gameBoard = [...initialGameBoard.map((array) => [...array] )];
+let winner;
 
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, column } = square;
+for (const turn of gameTurns) {
+  const { square, player } = turn;
+  const { row, column } = square;
 
-    gameBoard[row][column] = player;
+  gameBoard[row][column] = player;
+}
+
+for (const combination of WINNING_COMBINATIONS) {
+  const firstWinningSquare = gameBoard[combination[0].row][combination[0].column];
+  const secondWinningSquare = gameBoard[combination[1].row][combination[1].column];
+  const thirdWinningSquare = gameBoard[combination[2].row][combination[2].column];
+
+  if (firstWinningSquare && firstWinningSquare === secondWinningSquare && secondWinningSquare === thirdWinningSquare) {
+    winner = firstWinningSquare;
   }
+}
 
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstWinningSquare = gameBoard[combination[0].row][combination[0].column];
-    const secondWinningSquare = gameBoard[combination[1].row][combination[1].column];
-    const thirdWinningSquare = gameBoard[combination[2].row][combination[2].column];
+const hasDraw = gameTurns.length === 9 && !winner;
 
-    if (firstWinningSquare && firstWinningSquare === secondWinningSquare && secondWinningSquare === thirdWinningSquare) {
-      winner = firstWinningSquare;
-    }
-  }
+function handleSelectSquare(rowIndex, columnIndex) {
+  saveGameTurns(previousTurns => {
+    const currentPlayer = deriveActivePlayer(previousTurns);
 
-  const hasDraw = gameTurns.length === 9 && !winner;
+    const updatedTurns = [
+      { square: { row: rowIndex, column: columnIndex }, player: currentPlayer },
+      ...previousTurns
+    ];
 
-  function handleSelectSquare(rowIndex, columnIndex) {
-    saveGameTurns(previousTurns => {
-      const currentPlayer = deriveActivePlayer(previousTurns);
+    return updatedTurns;
+  });
+}
 
-      const updatedTurns = [
-        { square: { row: rowIndex, column: columnIndex }, player: currentPlayer },
-        ...previousTurns
-      ];
+function handleRematch() {
+  saveGameTurns([]);
+}
 
-      return updatedTurns;
-    });
-  }
-
-  return (
-    <main>
-      <div id="game-container">
-        <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
-        </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
-        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
-      </div>
-      <Log turns={gameTurns} />
-    </main>
-  )
+return (
+  <main>
+    <div id="game-container">
+      <ol id="players" className="highlight-player">
+        <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+        <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+      </ol>
+      {(winner || hasDraw) && <GameOver winner={winner} onRematch={handleRematch} />}
+      <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
+    </div>
+    <Log turns={gameTurns} />
+  </main>
+)
 }
 
 export default App
